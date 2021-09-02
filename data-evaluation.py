0cd9665753e6ca5a2f7f523a29796717c22180d5
giuseppe16180 <peppe98.marino@gmail.com>
2021-09-02 13:39:39 +0200

    test statistici
    Co-authored-by: Cristina Zappata <cristinazappata@users.noreply.github.com>

--- data-evaluation.py
+++ data-evaluation.py
@@ -1,4 +1,10 @@
 # %%
+
+from statsmodels.stats.multicomp import (pairwise_tukeyhsd, MultiComparison)
+from statsmodels.formula.api import ols
+import statsmodels.api as sm
+import scipy.stats as stats
+import seaborn as sns
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
@@ -8,25 +14,14 @@ import glob
 files = glob.glob("dataset/*.csv")
 
 df = pd.DataFrame()
-for f in files:
+for i, f in enumerate(files):
     csv = pd.read_csv(f, names=['sonification', 'time', 'color_dist',
-                                'target_color', 'clicked_color', 'target_size', 'clicked_size'], header=None)
+                                'target_color', 'clicked_color',
+                                'target_size', 'clicked_size'], header=None)
+    csv['subject'] = i
     df = df.append(csv)
 
-df.head()
-
-# %%
-
-corect = df[df['color_dist'] == 0]
-
-
-corect.groupby(corect['sonification'])['time'].median().head()
-
-
-# %%
-df[['sonification', 'time']].groupby('sonification').describe()
-# %%
-
+# %% definizione stili plot
 plt.style.use('fivethirtyeight')
 
 meanprops = {'marker': 'o',
@@ -51,7 +46,124 @@ flierprops = {'marker': 'o',
 
 capprops = {'linewidth': 1}
 
+position = [2, 0, 1]
+
+
+# %% tempo di esecuzione in base al colore
+my_cmap = plt.get_cmap("viridis")
+
+time_color = df[df["color_dist"] == 0].groupby(
+    "target_color").mean().reset_index()[['target_color', 'time']]
+
+step = 256 / 25
+
+plt.figure(figsize=(8, 6))
+plt.bar(np.round(time_color['target_color'] / step), time_color['time'], align='center', width=0.9,
+        color=my_cmap(time_color['target_color']))
+
+
+# %% calcolo medie per partecipanti di tempo e distanza
+mean_sub = df.groupby(['subject', 'sonification']).mean()[
+    ['time', 'color_dist']].reset_index().copy()
+
+# %% plot tempo medio dai partecipanti
+mean_sub.boxplot(figsize=(8, 6), by='sonification', column=['time'],
+                 showmeans=True,
+                 showfliers=False,
+                 meanprops=meanprops,
+                 boxprops=boxprops,
+                 whiskerprops=whiskerprops,
+                 medianprops=medianprops,
+                 flierprops=flierprops,
+                 capprops=capprops,
+                 positions=position)
+plt.title('')
+plt.suptitle('')
+plt.show()
+
+# %% plot distanza colore medio dai partecipanti
+mean_sub.boxplot(figsize=(8, 6), by='sonification', column=['color_dist'],
+                 showmeans=True,
+                 showfliers=False,
+                 meanprops=meanprops,
+                 boxprops=boxprops,
+                 whiskerprops=whiskerprops,
+                 medianprops=medianprops,
+                 flierprops=flierprops,
+                 capprops=capprops,
+                 positions=position)
+plt.title('')
+plt.suptitle('')
+plt.show()
+
 # %%
+
+mean_sonif = mean_sub.pivot(index=['subject'], columns=[
+                            'sonification']).reset_index().copy()
+
+# %%
+df.hist(figsize=(12, 8), by='sonification', column='time',
+        legend=True, bins=30, color='c', edgecolor='k', alpha=0.65)
+
+# %%
+time_mean = mean_sonif['time']
+fvalue, pvalue = stats.f_oneway(time_mean['n'], time_mean['s'], time_mean['b'])
+print(fvalue, pvalue)
+
+# %%
+MultiComp = MultiComparison(mean_sub['time'], mean_sub['sonification'])
+print(MultiComp.tukeyhsd().summary())
+comp = MultiComp.allpairtest(stats.ttest_rel, method='Bonferroni')
+print(comp[0])
+
+
+# %%
+mean_sonif['color_dist'].hist(figsize=(12, 8))
+# %%
+dist_mean = mean_sonif['color_dist']
+fvalue, pvalue = stats.f_oneway(dist_mean['n'], dist_mean['s'], dist_mean['b'])
+print(fvalue, pvalue)
+
+# %%
+MultiComp = MultiComparison(mean_sub['color_dist'], mean_sub['sonification'])
+print(MultiComp.tukeyhsd().summary())
+comp = MultiComp.allpairtest(stats.ttest_rel, method='Bonferroni')
+print(comp[0])
+
+
+# %% time ~ C(sonification) alla R
+model = ols('time ~ C(sonification)', data=mean_sub).fit()
+anova_table = sm.stats.anova_lm(model, typ=2)
+anova_table
+
+# %% time ~ C(sonification) alla R
+model = ols('color_dist ~ C(sonification)', data=mean_sub).fit()
+anova_table = sm.stats.anova_lm(model, typ=2)
+anova_table
+
+# %%
+
+
+# %%
+# res.anova_std_residuals are standardized residuals obtained from ANOVA (check above) sm.qqplot(res.anova_std_residuals, line='45')
+plt.xlabel("Theoretical Quantiles")
+plt.ylabel("Standardized Residuals")
+plt.show()
+# histogram
+plt.hist(res.anova_model_out.resid, bins='auto', histtype='bar', ec='k') plt.xlabel("Residuals")
+plt.ylabel('Frequency')
+plt.show()
+
+
+# %%
+corect = df[df['color_dist'] == 0]
+corect.groupby(corect['sonification'])['time'].median().head()
+
+# %%
+df[['sonification', 'time']].groupby('sonification').describe()
+
+
+# %% plot della distanza colore in base alla sonification
 
 df.boxplot(figsize=(8, 6), by='sonification', column=['color_dist'],
            showmeans=True,
@@ -61,9 +173,10 @@ df.boxplot(figsize=(8, 6), by='sonification', column=['color_dist'],
            whiskerprops=whiskerprops,
            medianprops=medianprops,
            flierprops=flierprops,
-           capprops=capprops)
+           capprops=capprops,
+           positions=position)
 
-# %%
+# %% plot del tempo in base alla sonification
 plot = df.boxplot(figsize=(8, 6), by='sonification', column=['time'],
                   showmeans=True,
                   showfliers=True,
@@ -72,24 +185,22 @@ plot = df.boxplot(figsize=(8, 6), by='sonification', column=['time'],
                   whiskerprops=whiskerprops,
                   medianprops=medianprops,
                   flierprops=flierprops,
-                  capprops=capprops)
-
-# %%
-
-
-my_cmap = plt.get_cmap("viridis")
+                  capprops=capprops,
+                  positions=position)
 
 
-time_color = df[df["color_dist"] == 0].groupby(
-    "target_color").mean().reset_index()[['target_color', 'time']]
-
-plt.figure(figsize=(8, 6))
-plt.bar(time_color['target_color'], time_color['time'], align='center', width=8,
-        color=my_cmap(time_color['target_color']))
-
-
-# %%
+# %% effetti della dimensione
 errors = df[df["color_dist"] != 0].copy()
 errors['pref_big'] = errors["target_size"] - errors['clicked_size']
 
 #errors.groupby(errors['pref_big'] < 0)['color_dist'].count().plot.bar()
+
+errors.boxplot(figsize=(8, 6), by='sonification', column=['pref_big'],
+               showmeans=True,
+               showfliers=True,
+               meanprops=meanprops,
+               boxprops=boxprops,
+               whiskerprops=whiskerprops,
+               medianprops=medianprops,
+               flierprops=flierprops,
+               capprops=capprops, positions=[2, 0, 1])
